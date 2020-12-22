463a2bb65625c0601478d72bc7a96429ffbaa9f2
Sajidah <sakth050@gold.ac.uk>
2020-12-22 17:02:09 +0000

    Updates

---
+++ precache-manifest.8bc283432013ad08d9395c66410d72ba.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd330fa1d6d0016ec695f7f91c8b0d3a",
+    "url": "/sajidahdev/index.html"
+  },
+  {
+    "revision": "07f1ee9de764abe03dea",
+    "url": "/sajidahdev/static/css/2.7b1f77b4.chunk.css"
+  },
+  {
+    "revision": "8665966600d5da8de2d6",
+    "url": "/sajidahdev/static/css/main.76f88402.chunk.css"
+  },
+  {
+    "revision": "07f1ee9de764abe03dea",
+    "url": "/sajidahdev/static/js/2.3c5821da.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/sajidahdev/static/js/2.3c5821da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8665966600d5da8de2d6",
+    "url": "/sajidahdev/static/js/main.a8452893.chunk.js"
+  },
+  {
+    "revision": "4b0689dbd7c88fab3293",
+    "url": "/sajidahdev/static/js/runtime-main.6a6cbb39.js"
+  },
+  {
+    "revision": "64a8711f9395e60ada4006943e11eed9",
+    "url": "/sajidahdev/static/media/Apercu-Light.64a8711f.otf"
+  },
+  {
+    "revision": "3d6dd91c6e9ab1f8b42c4ac4918eabde",
+    "url": "/sajidahdev/static/media/Canela-Light.3d6dd91c.woff"
+  },
+  {
+    "revision": "0249a3df988f210370bde52ae4a6add3",
+    "url": "/sajidahdev/static/media/arrow.0249a3df.svg"
+  },
+  {
+    "revision": "2612b99fa9c0fa5120e5d30114924d3f",
+    "url": "/sajidahdev/static/media/mobile-close.2612b99f.svg"
+  },
+  {
+    "revision": "39c559bf7a829464d3eaffd42b3edca4",
+    "url": "/sajidahdev/static/media/mobile-menu.39c559bf.svg"
+  },
+  {
+    "revision": "00f46a80020027254fb3803ba8f2cdfc",
+    "url": "/sajidahdev/static/media/preview.00f46a80.png"
+  },
+  {
+    "revision": "0138a858cf6e59c2ae2ffac8d12e38f2",
+    "url": "/sajidahdev/static/media/preview.0138a858.png"
+  },
+  {
+    "revision": "1aca2fa7640214e22b7446c4edb0d489",
+    "url": "/sajidahdev/static/media/preview.1aca2fa7.png"
+  },
+  {
+    "revision": "766390bea604f67958b843558f481803",
+    "url": "/sajidahdev/static/media/preview.766390be.png"
+  },
+  {
+    "revision": "7778201959466993454f01ee62f053d1",
+    "url": "/sajidahdev/static/media/preview.77782019.gif"
+  },
+  {
+    "revision": "7ed55111a28c9385901247fffcde57f8",
+    "url": "/sajidahdev/static/media/preview.7ed55111.png"
+  },
+  {
+    "revision": "d0fae49ab718f24ef6b154dec4af0dab",
+    "url": "/sajidahdev/static/media/preview.d0fae49a.png"
+  },
+  {
+    "revision": "ced8365e41b274e4c304d948591c63ee",
+    "url": "/sajidahdev/static/media/star.ced8365e.svg"
+  },
+  {
+    "revision": "76ae75036726fa7648a3e9ec604e632d",
+    "url": "/sajidahdev/static/media/up-arrow.76ae7503.svg"
+  }
+]);
